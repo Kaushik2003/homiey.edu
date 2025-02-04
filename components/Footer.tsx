21ba22d249304fa2a3ed60e286cea2714630509f
Debanjan Mondal <debanjanmondalk2005@gmail.com>
2025-02-04 22:49:31 +0530

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { Linkedin, Twitter, Youtube } from "lucide-react"
+import { GithubIcon } from "lucide-react"
 
 export default function Footer() {
   return (
@@ -8,9 +8,13 @@ export default function Footer() {
         <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center text-gray-400 text-sm">
           <span>&copy; 2025 googlesolution.ai</span>
           <div className="flex space-x-6">
-            <Link href="#" className="hover:text-white">Contact</Link>
-            <Link href="#" className="hover:text-white">Twitter</Link>
-            <Link href="#" className="hover:text-white">Github</Link>
+            {/* <Link href="#" className="hover:text-white">Contact</Link>
+            <Link href="#" className="hover:text-white"><X/></Link> */}
+            <Link href="#" className="hover:text-white"><button className="shadow-[inset_0_0_0_2px_#616467] text-black px-8 py-2 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
+            <GithubIcon />
+            </button>    
+            </Link>
+
           </div>
         </div>
       </div>
