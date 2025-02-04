2f2df500fb2a5f5a3fb73e520a56bcd5f584be58
Kaushik Samadder <70917815+Kaushik2003@users.noreply.github.com>
2025-02-05 03:51:12 +0530

    Fixed the second bento ui component

--- components/Features.tsx
+++ components/Features.tsx
@@ -6,13 +6,13 @@ import { HelpCircle, BookOpen, FileText, FileSearch } from "lucide-react";
 import { motion } from "framer-motion";
 import Lottie from "@lottielab/lottie-player/react";
 import { Meteors } from "../components/ui/meteors";
-
+import Image from "next/image";
 
 
 
 export default function BentoGridCustomDemo() {
   return (
-    <BentoGrid className="max-w-6xl pb-20 mx-auto md:auto-rows-[20rem] grid gap-6">
+    <BentoGrid className="max-w-6xl pb-20 mx-auto md:auto-rows-[20rem] grid gap-5">
       {items.map((item, i) => (
         <BentoGridItem
           key={i}
@@ -27,8 +27,105 @@ export default function BentoGridCustomDemo() {
   );
 }
 
-const SkeletonOne = () => (
+const SkeletonOne = () => {
   // <motion.div className="flex flex-1 w-full h-full min-h-[6rem] p-4 bg-neutral-200 dark:bg-neutral-800 rounded-xl border border-neutral-300 dark:border-neutral-700" />
+  const variants = {
+    initial: {
+      x: 0,
+    },
+    animate: {
+      x: 10,
+      rotate: 5,
+      transition: {
+        duration: 0.2,
+      },
+    },
+  };
+  const variantsSecond = {
+    initial: {
+      x: 0,
+    },
+    animate: {
+      x: -10,
+      rotate: -5,
+      transition: {
+        duration: 0.2,
+      },
+    },
+  };
+  
+  return (
+    <motion.div
+      initial="initial"
+      whileHover="animate"
+      className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-col space-y-2"
+    >
+      <motion.div
+        variants={variants}
+        className="flex flex-row rounded-2xl border border-neutral-100 dark:border-white/[0.2] p-2  items-start space-x-2 bg-white dark:bg-black"
+      >
+        <Image
+          src="https://i.pinimg.com/736x/d4/76/8c/d4768c0e7d8762294c1f169a6a1d3c27.jpg"
+          alt="avatar"
+          height="100"
+          width="100"
+          className="rounded-full h-10 w-10"
+        />
+        <p className="text-xs text-neutral-500">
+        How do plants produce their own food through photosynthesis?
+        </p>
+      </motion.div>
+      <motion.div
+        variants={variantsSecond}
+        className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2 items-center justify-end space-x-2 w-3/4 ml-auto bg-white dark:bg-black"
+      >
+        
+        <p className="text-xs text-neutral-500">Using sunlight, water and CO2</p>
+        {/* <div className="h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-violet-500 flex-shrink-0" /> */}
+        <Image
+          src="https://i.pinimg.com/736x/f1/7d/db/f17ddb244e3f2f6a720e61cd3f8161fb.jpg"
+          alt="avatar"
+          height="100"
+          width="100"
+          className="rounded-full h-10 w-10"
+        />
+      </motion.div>
+    </motion.div>
+  );
+
+};
+
+const SkeletonTwo = () => (
+  <Lottie src="https://cdn.lottielab.com/l/AjBDKht8s1oUa0.json" autoplay />
+);
+
+const SkeletonThree = () => (
+  <motion.div
+    className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] rounded-lg bg-dot-black/[0.2] flex-col space-y-2 p-4"
+    style={{
+      background: "linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)",
+      backgroundSize: "400% 400%",
+    }}
+  />
+);
+
+const SkeletonFour = () => (
+  
+  <Lottie src="https://cdn.lottielab.com/l/CJZCvDA8pLuwCZ.json" autoplay />
+  
+);
+
+// const SkeletonFive = () => (
+//   <motion.div className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-col space-y-2 p-4">
+//     <motion.div className="flex flex-row rounded-2xl border border-neutral-100 dark:border-white/[0.2] p-4 items-start space-x-2 bg-white dark:bg-black">
+//       <div className="h-8 w-10 rounded-full bg-gradient-to-r from-pink-500 to-violet-500" />
+//       <div className="w-full bg-gray-100 h-4 rounded-full dark:bg-neutral-900">Hi</div> 
+//     </motion.div>
+//   </motion.div>
+// );
+
+
+const SkeletonFive = () => (
   <div className="">
       <div className=" w-full h-full relative max-w-xs">
         <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-purple-500 to-blue-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
@@ -58,36 +155,6 @@ const SkeletonOne = () => (
         </div>
       </div>
     </div>
-
-);
-
-const SkeletonTwo = () => (
-  <Lottie src="https://cdn.lottielab.com/l/AjBDKht8s1oUa0.json" autoplay />
-);
-
-const SkeletonThree = () => (
-  <motion.div
-    className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] rounded-lg bg-dot-black/[0.2] flex-col space-y-2 p-4"
-    style={{
-      background: "linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)",
-      backgroundSize: "400% 400%",
-    }}
-  />
-);
-
-const SkeletonFour = () => (
-  
-  <Lottie src="https://cdn.lottielab.com/l/CJZCvDA8pLuwCZ.json" autoplay />
-  
-);
-
-const SkeletonFive = () => (
-  <motion.div className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-col space-y-2 p-4">
-    <motion.div className="flex flex-row rounded-2xl border border-neutral-100 dark:border-white/[0.2] p-4 items-start space-x-2 bg-white dark:bg-black">
-      <div className="h-10 w-10 rounded-full bg-gradient-to-r from-pink-500 to-violet-500" />
-      <div className="w-full bg-gray-100 h-4 rounded-full dark:bg-neutral-900" />
-    </motion.div>
-  </motion.div>
 );
 
 const items = [
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'pbs.twimg.com',
+          port: '',
+          pathname: '/account123/**',
+          search: '',
+        },
+      ],
         domains: ["i.pinimg.com"],
       },
 };
