75fccdea3f1dbd2310cddbf474919a23fc5d77ee
Debanjan Mondal <debanjanmondalk2005@gmail.com>
2025-02-04 22:00:24 +0530

    adad

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-black">
       <Header />
-      <main>
+      <main >
         <Hero />
         <Features />
         <FAQ />
